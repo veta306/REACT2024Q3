5354270c312b987282ee8563b5924760443f536d
veta306 <vv097099@gmail.com>
2024-07-08 09:45:52 +0300

    refactor: specify api response type

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,17 @@
+import Film from "../types/Film";
 const URL = "https://swapi.dev/api/films/";
+
+interface ApiResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: Film[];
+}
 
 export default async function fetchFilms(searchPhrase: string) {
   const result = searchPhrase
     ? await fetch(URL + "?search=" + searchPhrase)
     : await fetch(URL);
-  const films = await result.json();
+  const films: ApiResponse = await result.json();
   return films.results;
 }
